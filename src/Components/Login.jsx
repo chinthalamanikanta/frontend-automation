024f531ab90590df1fe227f68260a337acc90771
Yugander <yugandhar@middlewaretalents.com>
2025-02-06 15:28:24 +0530

    ravi

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
 
         try {
             // Send login request to the backend
-            const response = await axios.post('https://talents-backend.azurewebsites.net/api/v1/employeeManager/login', {
+            const response = await axios.post('http://4.248.241.255/api/v1/employeeManager/login', {
                 email: email,
                 password: password,
             });
