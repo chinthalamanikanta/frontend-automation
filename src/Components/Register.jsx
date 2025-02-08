d84d2d5245ec7d15ad6a457a14756982dda0bedc
chinthalamanikanta <143411132+chinthalamanikanta@users.noreply.github.com>
2025-02-08 15:43:26 +0530

    Update Register.jsx

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -95,7 +95,7 @@ const Register = () => {
 
         try {
             console.log("Sending registration request to API"); // Debug message
-            const response = await axios.post("http://4.248.241.255/api/v1/employeeManager/register", formData, {
+            const response = await axios.post("http://4.230.147.91/api/v1/employeeManager/register", formData, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
                 }
